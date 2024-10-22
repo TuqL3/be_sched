b5952550881b6820a1d8ce2bd58c2d942be3e2fc
tung.lv <tung.lv@tinasoft.com.vn>
2024-10-22 15:34:58 +0700

    fix cors

--- main.go
+++ main.go
@@ -14,13 +14,13 @@ import (
 func main() {
 	config.LoadEnv()
 	config.PostgresConnection()
-	router := gin.New()
+	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Authorization"},
 		AllowCredentials: true,
 	}))
 
