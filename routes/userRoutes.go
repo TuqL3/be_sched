761885d74dec5747ccbda5d405c2cc53cdc3d7f8
TuqL3 <tungledxh@gmail.com>
2024-11-25 15:08:00 +0700

    fix

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -17,7 +17,7 @@ func UserRoute(route *gin.Engine, controller *controllers.UserController) {
 	userRouteMiddleware := route.Group("/api/v1/user")
 	{
 
-		userRouteMiddleware.PUT("/update/:userId", middleware.RolePermissionMiddleware([]string{"admin"}, []string{"modifyUser"}), controller.UpdateUser)
+		userRouteMiddleware.PUT("/update/:userId", controller.UpdateUser)
 		userRouteMiddleware.DELETE("/delete/:userId", middleware.RolePermissionMiddleware([]string{"admin"}, []string{"deleteUser"}), controller.DeleteUser)
 		userRouteMiddleware.GET("/getcountuser", middleware.RolePermissionMiddleware([]string{"admin"}, []string{"viewUser"}), controller.GetCountUser)
 		userRouteMiddleware.GET("/profile", middleware.RolePermissionMiddleware([]string{"admin", "giangvien", "trucban", "giamdoc"}, []string{"viewProfile"}), controller.GetAllUsers)
